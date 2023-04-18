4d1b5c4f19e026c65c4b667625f4e398f8d670dc
BigPlanes <129475949+BigPlanes@users.noreply.github.com>
2023-04-18 16:43:42 +0800

    王者战力_Bncr_v1.0.1
    平台更换为序号选择

--- 王者战力_Bncr.js
+++ 王者战力_Bncr.js
@@ -2,36 +2,43 @@
  * @author 薛定谔的大灰机
  * @name 王者战力
  * @origin 大灰机
- * @version 1.0.0
+ * @version 1.0.1
  * @description 查询王者战力
  * @platform tgBot qq ssh HumanTG wxQianxun wxXyo
+ * @rule ^王者战力$
  * @rule ^战力$
- * @rule ^战力 ([^ \n]+)$
- * @rule ^战力 ([^ \n]+) ([^ \n]+)$
+ * @rule ^([^ \n]+)战力$
+ * @rule ^战力([^ \n]+)$
+ * @rule ^战力([^ \n]+)([^ \n]+)$
  * @admin false
  * @disable false
  */
 
+const mo = require('./mod/subassembly')      // 此脚本依赖仓库模块，请拉取全部文件
 const axios = require("axios")
 
 module.exports = async s => {
     let msg_wait = 2
 
-    // console.log(`${s.param(1)}\n${s.param(2)}\n${s.param(3)}`);
-
+    types = [
+        "aqq",
+        "awx",
+        "iqq",
+        "iwx",
+    ]
     let api = `https://www.sapi.run/hero/select.php`
     if (!(hero = s.param(1))) {
         msgid_ids = await s.reply(`输入英雄名`)
         if (!(hero = await talks())) return
     }
     if (!(type = s.param(2))) {
-        msgid_ids = await s.reply(`平台任选一\naqq、awx、iqq、iwx`)
-        if (!(type = await talks())) return
+        msgid_ids = await s.reply(`输入序号：\n1：安卓QQ\n2：安卓微信\n3：苹果QQ\n4：苹果微信`)
+        if (!(type = types[(await talks()) - 1])) return
     }
     s.delMsg(s.getMsgId())
     let url = `${api}?hero=${encodeURI(hero)}&type=${type}`
     let data = (await get(url)).data
-    console.log(data)
+    // console.log(data)
     if (data && data.code == 200) {
         msg = `英雄名称：${data.data.alias}`
         msg += `\n平台大区：${data.data.platform}`
@@ -43,23 +50,25 @@ module.exports = async s => {
         path = data.data.photo
     } else {
         msg = data.msg
+        path = ``
     };
-    console.log(msg);
-    ["tgBot", "HumanTG"].includes(s.getFrom()) && s.reply({ type: "image", path: path, msg: msg });
-    if (["wxXyo", "wxKeAImao", "wxQianxun", "qq"].includes(s.getFrom())) {
-        await s.reply(msg)
-        await s.reply({
-            type: "image",
-            path: path
-        })
-    }
+    // console.log(msg);
+    await mo.sendMsg(s, {
+        type: `image`,
+        msg: msg,
+        path: {
+            path: path,
+            suffix: `jpg`,
+        },
+    })
     async function talks() {
         let content = await s.waitInput(async (s) => {
             let msg = s.getMsg();
             if (msg === 'q') {
             } else if (!msg || (((await get(`${api}?hero=${encodeURI(hero || msg)}&type=aqq`)).data.code) == 400)) {
-                s.delMsg(await s.reply('错误,重新输出'), { wait: msg_wait })
-                return 'again'
+                s.delMsg(await s.reply('错误'), { wait: msg_wait })
+                // return 'again'
+                return
             }
         }, 30);
         if (content === null) return s.delMsg(await s.reply('超时已退出'), msgid_ids, { wait: msg_wait });
