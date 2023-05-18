560ca8bfb9f1d4449af4a1a39c1d25cf0ccae0bb
Hugo Chiang <hugo@gyroflow.xyz>
2023-05-18 08:46:47 +0800

    Create RouterOS重拨_Bncr.js

---
+++ RouterOS重拨_Bncr.js
@@ -0,0 +1,59 @@
+/**作者
+ * @author 薛定谔的大灰机, DusKing
+ * @name RouterOS
+ * @origin 大灰机
+ * @version 1.0.0
+ * @description RouterOS重拨
+ * @platform tgBot qq ssh HumanTG wxQianxun wxXyo
+ * @rule ros重拨
+ * @admin true
+ * @disable false
+ */
+
+sysMethod.testModule(['ssh2'], { install: true });
+const Client = require('ssh2').Client;
+// SSH参数
+const host = '10.0.0.1' // 路由器IP
+const port = 22         // SSH端口
+const username = ''     // SSH用户名
+const password = ''     // SSH密码
+const command = '/interface disable pppoe-out1;/interface enable pppoe-out1'    // 执行命令
+
+module.exports = async s => {
+    try {
+        output = await sshExecCommand(host, port, username, password, command);
+        console.log("重拨结果:" + ":" + output.replace("\n", "").replace("Done", "成功"));
+        await s.reply("重拨结果:" + ":" + output.replace("\n", "").replace("Done", "成功"));
+    } catch (error) {
+        console.error(error);
+        await s.reply(error);
+    }
+}
+
+async function sshExecCommand(host, port, username, password, command) {
+    return new Promise((resolve, reject) => {
+        conn = new Client();
+        conn.on('ready', () => {
+            conn.exec(command, (err, stream) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                let output = '';
+                stream.on('close', (code, signal) => {
+                    conn.end();
+                    resolve(output);
+                }).on('data', (data) => {
+                    output += data.toString();
+                }).stderr.on('data', (data) => {
+                    console.error('STDERR: ' + data);
+                });
+            });
+        }).connect({
+            host: host,
+            port: port,
+            username: username,
+            password: password
+        });
+    });
+}
