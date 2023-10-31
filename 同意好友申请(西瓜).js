19a3d25728e6ef52f557ac5e8c073cfc86b38f73
BigPlanes <129475949+BigPlanes@users.noreply.github.com>
2023-10-31 12:29:05 +0800

    Update 同意好友申请(西瓜).js
    新增关键词拉群

--- 同意好友申请(西瓜).js
+++ 同意好友申请(西瓜).js
@@ -2,9 +2,10 @@
  * @author 薛定谔的大灰机
  * @name 同意好友申请
  * @origin 大灰机
- * @version 1.0.0
- * @description 
+ * @version 1.0.1
  * @rule 收到好友添加请求
+ * @description 下方加群关键词可自定义
+ * @rule 加群
  * @platform wxXyo
  * @priority 99999
  * @admin false
@@ -52,53 +53,78 @@
 
  */
 
-const Agree = true              // 是否同意申请
-const Agree_keyword = ''        // 关键词同意
+// 是否同意申请
+const Agree = true
+// 关键词同意
+const Agree_keyword = ''
+// 是否自动回复
+const Auto_reply = true
+// 自动回复内容
+const Auto_reply_msg = '感谢添加'
 
-const Auto_reply = true         // 自动回复
-const Auto_reply_msg = '感谢添加'         // 自动回复
+// 群ID
+const GroupId = '25214210457'
+// 拉群方式（InviteInGroupByLink是发送链接，InviteInGroup是直接拉）
+const mode = 'InviteInGroupByLink'
 
 module.exports = async s => {
-    const body = s.Bridge.body.content
-
-    if (body && s.getUserId() == 'EventFrieneVerify') {
-        console.log(`收到【${body.from_name}】好友申请`)
-        await main()
+    if (s.param(1) != '收到好友添加请求') {
+        Group()
     } else {
-        console.log('非真实好友申请，忽略')
+        Friend()
     }
 
-    async function main() {
-        if (Agree) {
-            await AgreeFriendVerify()
-            if (Auto_reply) {
-                await auto_reply()
-            }
-        }
-    }
-
-    async function AgreeFriendVerify() {
-        if (Agree_keyword) {
-            if (Agree_keyword !== body.json_msg.content) {
-                return console.log('好友申请关键词错误')
-            }
-        }
+    async function Group() {
         await s.reply({
-            type: 'friend',
+            type: 'Group',
             msg: '同意好友申请',
-            fromType: body.type,
-            json_msg: body.json_msg
+            api: mode,
+            group_wxid: GroupId + '@chatroom',
+            friend_wxid: s.getUserId()
         })
-        console.log('已同意好友申请')
     }
 
-    async function auto_reply() {
-        if (Auto_reply_msg) {
+    async function Friend() {
+        const body = s.Bridge.body.content
+        if (body && s.getUserId() == 'EventFrieneVerify') {
+            console.log(`收到【${body.from_name}】好友申请`)
+            await main()
+        } else {
+            console.log('非真实好友申请，忽略')
+        }
+
+        async function main() {
+            if (Agree) {
+                await AgreeFriendVerify()
+                if (Auto_reply) {
+                    await auto_reply()
+                }
+            }
+        }
+
+        async function AgreeFriendVerify() {
+            if (Agree_keyword) {
+                if (Agree_keyword !== body.json_msg.content) {
+                    return console.log('好友申请关键词错误')
+                }
+            }
             await s.reply({
-                type: 'text',
-                userId: body.from_wxid,
-                msg: Auto_reply_msg,
+                type: 'friend',
+                msg: '同意好友申请',
+                fromType: body.type,
+                json_msg: body.json_msg
             })
+            console.log('已同意好友申请')
+        }
+
+        async function auto_reply() {
+            if (Auto_reply_msg) {
+                await s.reply({
+                    type: 'text',
+                    userId: body.from_wxid,
+                    msg: Auto_reply_msg,
+                })
+            }
         }
     }
 }
